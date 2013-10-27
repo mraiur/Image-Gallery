15b56b2dd738f23ed9168cf770e7df794e8bf028
Nikolai Ivanov <mraiur@nikolai-ivanov.com>
2013-10-27 21:53:58 +0200

    Added sizes in the example config.

--- core/config.example.php
+++ core/config.example.php
@@ -3,6 +3,12 @@ if(!defined("app")){
     header("Location: index.php");
 }
 
+$image_sizes = array(
+    1 => array('width' => 750, 'height' => 600), // small
+    2 => array('width' => 1000, 'height' => 768), // medium
+    3 => array('width' => 1550, 'height' => 1200) // large
+);
+
 $users = array(
     // username => array("password" => MD5(PASSWORD))
     "" => array("password" => "")
